256c6c4e1fea82814ee00fad1c2f154fdf86a474
Antonio Lorenzo Vizcaino <lorenzovizcaino@gmail.com>
2023-10-18 19:41:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sumasrestas/ViewBindingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sumasrestas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewBindingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_binding)
+    }
+}
