6a6e0f9348640e26cbce9ba7214d67bd99a90564
Antonio Lorenzo <lorenzovizcaino@gmail.com>
2023-10-22 03:02:08 +0200

    22/10/2023 03:01

--- app/src/main/java/com/example/sumasrestas/ViewBindingActivity.kt
+++ app/src/main/java/com/example/sumasrestas/ViewBindingActivity.kt
@@ -2,10 +2,42 @@ package com.example.sumasrestas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.sumasrestas.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ViewBindingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_binding)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalcular.setOnClickListener {
+            val entero1=binding.cajanum1.text.toString().toInt()
+            val entero2=binding.cajanum2.text.toString().toInt()
+            if(binding.sumar.isChecked) {
+                val suma=entero1+entero2
+                binding.resultado.text = "$suma"
+
+            }else{
+                val resta=entero1-entero2
+                binding.resultado.text = "$resta"
+            }
+        }
+        binding.titulo.setOnClickListener {
+            Toast.makeText(this,R.string.title,Toast.LENGTH_LONG).show()
+        }
+
+        binding.resultado.setOnClickListener {
+            Snackbar.make(binding.sumar,"Aqui Calculo Resultado",Snackbar.LENGTH_LONG)
+                .setAction("Sumando"){
+                    println("HOLA")
+                    binding.sumar.isActivated=true
+                }.show()
+        }
+
     }
 }
